5360cdee91e2df958827b7ca944b8ee666152d44
Andrei <a.vitiuc@gmail.com>
2023-05-18 12:59:16 -0400

    javascript objects and statement

--- practice1_variables.js
+++ practice1_variables.js
@@ -20,6 +20,9 @@ console.log('Hello World in single quote');
 
 let message = "Hello to javascript"; 
 console.log(message);
+// re-assigning value 
+message = "Javascript is fun !";
+console.log(message);
 
 var num = 10; 
 console.log(num);
@@ -35,3 +38,10 @@ console.log(coffee);
 
 let negativity = null; 
 console.log(negativity);
+
+// creating a variable that can not change 
+// const -> short for constant , a value that does not change 
+
+const bla = "John"; 
+//  bla = "something else";  // this will throw an error , can not re-assign the const 
+console.log(bla);
