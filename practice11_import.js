793ad45e877449914ee2a7ce76b094f03213167f
Andrei <a.vitiuc@gmail.com>
2023-05-22 15:38:04 -0400

    javascript practice

---
+++ practice11_import.js
@@ -0,0 +1,11 @@
+import {sayHelloTo, addNumbers} from "./practice11_methods.js";
+
+import chalk from "chalk";
+import 
+sayHelloTo("John");
+sayHelloTo("Marina");
+
+console.log(addNumbers(100,200));
+
+console.log(chalk.blue("Hello World"));
+
