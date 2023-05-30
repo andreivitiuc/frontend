cb86dfddb327a98446aa6f58b00ded899e20ca70
Andrei <a.vitiuc@gmail.com>
2023-05-30 15:28:28 -0400

    updated

---
+++ practice_waiting.js
@@ -0,0 +1,16 @@
+// in javascript you can pause the execution with fix time and then move on to next
+
+// print Hello
+// wait 3 seconds print LWC
+// print World
+console.log('Hello');
+// setTimeout method is used for pausing execution
+// it accepts 2 parameters
+// 1 : a function that accepts no param and provide logic
+// 2 : time you want to wait in millisecond, 1s = 1000ms
+
+setTimeout(()=>{
+    console.log('LWC');
+
+},300);
+console.log('World');
